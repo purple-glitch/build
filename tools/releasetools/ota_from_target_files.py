c51535bc8b1ffb419dc3e2eab5be7776a5b66ab5
SuperDroidBond <superdroidbond@yahoo.com>
2018-08-25 21:19:31 +0530

    Add ASCII Art, HW & SW Info in Flashing Script
    Ref:- https://github.com/AICP/build/commit/51eb7dd

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -982,6 +982,47 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   system_progress = 0.75
 
+  script.Print("************************************************");
+  script.Print("************************************************");
+  script.Print("             Be Ready To Get Xtended            ");
+  script.Print("************************************************");
+  script.Print("                                                ");
+  script.Print("XX      XX                                      ");
+  script.Print(" XX    XX  TTTTT EEEE NN   N DDD   EEEEE DDD    ");
+  script.Print("  XX  XX     T   E    N N  N D  D  E     D   D  ");
+  script.Print("    XX       T   EEEE N  N N D   D EEEEE D    D ");
+  script.Print("  XX  XX     T   E    N   NN D  D  E     D   D  ");
+  script.Print(" XX    XX    T   EEEE N    N DDD   EEEEE DDD    ");
+  script.Print("XX      XX         CUSTOM ROM REDEFINED         ");
+  script.Print("************************************************");
+  script.Print("        MSM Xtended ROM by Team-Xtended         ");
+  script.Print("************************************************");
+
+  if target_info.GetBuildProp("ro.xtended.version") is not None:
+    buildid = target_info.GetBuildProp("ro.xtended.version")
+    androidver = target_info.GetBuildProp("ro.build.version.release")
+    buildtype = target_info.GetBuildProp("ro.build.type")
+    buildidn = target_info.GetBuildProp("ro.build.id")
+    buildday = target_info.GetBuildProp("ro.build.date")
+    securep = target_info.GetBuildProp("ro.build.version.security_patch")
+    device = target_info.GetBuildProp("ro.product.name")
+    manufacturer = target_info.GetBuildProp("ro.product.manufacturer")
+  script.Print("***********************************************");
+  script.Print(" ROM version      : %s"%(buildid));
+  script.Print("");
+  script.Print(" Android version  : %s"%(androidver));
+  script.Print("");
+  script.Print(" Security patch   : %s"%(securep));
+  script.Print("");
+  script.Print(" Build date       : %s"%(buildday));
+  script.Print("");
+  script.Print(" Build type       : %s"%(buildtype));
+  script.Print("");
+  script.Print("***********************************************");
+  script.Print(" Device           : %s"%(device));
+  script.Print(" Manufacturer     : %s"%(manufacturer));
+  script.Print("***********************************************");
+
   if OPTIONS.wipe_user_data:
     system_progress -= 0.1
   if HasVendorPartition(input_zip):
@@ -1046,6 +1087,8 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
+
+  script.Print("Your Phone just got XTENDED");
 
   if OPTIONS.extra_script is not None:
     script.AppendExtra(OPTIONS.extra_script)
