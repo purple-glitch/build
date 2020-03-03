bbf591845ba13fd6f5600bc67b842e63e62c7933
purple-glitch <60851364+purple-glitch@users.noreply.github.com>
2020-03-03 21:21:39 +0530

    Update soong_config.mk

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -176,7 +176,7 @@ $(foreach namespace,$(SOONG_CONFIG_NAMESPACES),\
   $(call end_json_map))
 $(call end_json_map)
 
-include vendor/xtended/build/soong/soong_config.mk
+include vendor/ee/build/soong/soong_config.mk
 
 $(call json_end)
 
